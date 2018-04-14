510843e3edc2fc6c13db494b8c0437c485d0b500
Julian J. Shun <jshun@lanka.csail.mit.edu>
2018-04-14 17:03:34 -0400

    added myVector.h"

---
+++ myVector.h
@@ -0,0 +1,30 @@
+#pragma once
+
+struct myVector {
+  long maxSize, end;
+  uintT *A;
+  void init() {
+    maxSize = 1;
+    end = 0;
+    A = newA(uintT, maxSize);
+  }
+  void reset() {
+    end = 0;
+  }
+  void resize() {
+    uintT *B = newA(uintT, 2 * maxSize);
+    for (long i = 0; i < maxSize; i++) {
+      B[i] = A[i];
+    }
+    // free(A); //should free
+    A = B;
+    maxSize *= 2;
+  }
+  void add(uintT v) {
+    if (end == maxSize)
+      resize();
+    A[end++] = v;
+  }
+  uintT size() { return end; }
+  uintT get(uintT i) { return A[i]; }
+};
