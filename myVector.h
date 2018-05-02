5699184a4de0a0baac25bcf3f492073fbff663f3
Joana Trindade <jmftrindade@gmail.com>
2018-05-02 14:21:28 -0400

    support deletions in myVector.h

--- myVector.h
+++ myVector.h
@@ -1,29 +1,46 @@
 #pragma once
 
 struct myVector {
-  long maxSize, end;
+  long maxSize, end, numRemoved;
   uintT *A;
   void init() {
     maxSize = 1;
     end = 0;
+    numRemoved = 0;
     A = newA(uintT, maxSize);
   }
   void reset() {
     end = 0;
   }
-  void resize() {
-    uintT *B = newA(uintT, 2 * maxSize);
+  void resize(long targetSize) {
+    uintT *B = newA(uintT, targetSize);
     for (long i = 0; i < maxSize; i++) {
-      B[i] = A[i];
+      // skip removed values
+      if (A[i] != -1) {
+        B[i] = A[i];
+      }
     }
-    free(A); 
+    free(A);
     A = B;
-    maxSize *= 2;
+    maxSize = targetSize;
   }
   void add(uintT v) {
-    if (end == maxSize)
-      resize();
+    if (end == maxSize) {
+      resize(maxSize * 2);
+    }
     A[end++] = v;
+  }
+  // removes first occurrence of value v
+  void remove(uintT i) {
+    // -1 is dummy value used as deletion marker.
+    if (A[i] != -1) {
+      A[i] = -1;
+      numRemoved++;
+    }
+    // trigger garbage collection if too many have been removed.
+    if (numRemoved > (size() / 2)) {
+      resize(maxSize / 2);
+    }
   }
   uintT size() { return end; }
   uintT get(uintT i) { return A[i]; }
