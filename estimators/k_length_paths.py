1b750f9520e678ccb48c646f8160b96364d4dd25
Joana M. F. da Trindade <jmf@csail.mit.edu>
2018-07-18 02:45:03 -0400

    estimator

--- estimators/k_length_paths.py
+++ estimators/k_length_paths.py
@@ -2,10 +2,20 @@ import argparse
 import json
 import networkx as nx
 import numpy as np
+import scipy
+from scipy.special import binom
+
+# (n choose (k+1)) * (m/(n choose 2))^k
+def lower_bound(n, m, k):
+  num_possible_k_hop_paths = binom(n, k+1)
+  num_possible_edges = binom(n, 2)
+  num_1_hop_paths = float(m) / float(num_possible_edges)
+
+  return int(num_possible_k_hop_paths * pow(num_1_hop_paths, k))
 
 
 # Estimates number of k-length paths.
-def k_length_paths(k_hop_schema, config):
+def k_length_paths(n, m, k_hop_schema, config):
     k = len(k_hop_schema)
     k_length_paths = {}  # One for each value of alpha.
 
@@ -35,26 +45,31 @@ def k_length_paths(k_hop_schema, config):
           k_length_paths[alpha] += int(float(n) * factor
               * pow(deg_summary, k))
 
+    # Also estimate using lower bound formula.
+#    k_length_paths['lower_bound'] = lower_bound(n, m, k)
+
     return k_length_paths
 
 
-def main(k_hop_schema, input_outdegree_files):
+def main(num_nodes, num_edges, k_hop_schema, input_outdegree_files):
     print k_hop_schema
     config=json.loads(input_outdegree_files)
 
     print 'Using het. network estimator=%s' % k_length_paths(
-        k_hop_schema, config)
+        num_nodes, num_edges, k_hop_schema, config)
 
 
 # e.g.,
-# python k_length_paths.py -s author article author -i \
-# '{"author": \
+# python k_length_paths.py -m 10000 -s author article author
+# -i '{"author": \
 #   "/big_fast_drive/jfon/nets/1graphdblp/outdeg_author-10k.txt", \
 #   "article": \
 #   "/big_fast_drive/jfon/nets/1graphdblp/outdeg_article-10k.txt"}'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Estimate number of k-length paths in heterogeneous net.')
+    parser.add_argument('-m', '--num_edges', type=int, required=True)
+    parser.add_argument('-n', '--num_nodes', type=int, required=True)
     parser.add_argument('-s', '--k_hop_schema',
         help='Space separated list, e.g., author article author',
         nargs='+',
