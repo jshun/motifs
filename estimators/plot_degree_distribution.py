2989218756f4cd1a0af442a374ae90d193dd4152
Joana M. F. da Trindade <jmf@csail.mit.edu>
2018-07-19 01:30:13 -0400

    outdegree dist

--- estimators/plot_degree_distribution.py
+++ estimators/plot_degree_distribution.py
@@ -34,9 +34,9 @@ def round_up(x):
   return int(10**(np.ceil(np.log10(x))))
 
 
-def plot_cdfs(data, output_suffix):
+def plot_cdfs(data, title):
     # CDF
-    output_filename = 'cdf-' + output_suffix
+    output_filename = 'cdf-%s.pdf' % title
     s = float(data.sum())
     cdf = data.cumsum(0)/s
     plt.figure()
@@ -48,6 +48,7 @@ def plot_cdfs(data, output_suffix):
     plt.xscale('symlog')
     ax = plt.gca()
     ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+    plt.title(title)
     plt.ylabel('CDF')
     plt.xlabel('Degree')
     fig = plt.gcf()
@@ -58,7 +59,7 @@ def plot_cdfs(data, output_suffix):
     print 'CDF saved as %s' % output_filename
 
     # CCDF
-    output_filename = 'ccdf-' + output_suffix
+    output_filename = 'ccdf-%s.pdf' % title
     ccdf = 1-cdf
     freq = ccdf * s
     plt.figure()
@@ -73,15 +74,15 @@ def plot_cdfs(data, output_suffix):
     first_rank = 2
     log_x = log_x[first_rank:]
     log_y = log_y[first_rank:]
-
     # Don't try to fit if we don't have at least 2 points
     if len(log_x) > 2:
         m, b = np.polyfit(log_x, log_y, 1)  # log(y) = m*log(x) + b
         y_fit = np.power(10, m * log_x + b)
-        plt.plot(range(first_rank, len(log_x) + first_rank), y_fit, ':')
-
+        plt.plot(range(first_rank, len(log_x) + first_rank),
+            y_fit, '--', color='red', linewidth=3.0, alpha=1.0)
     max_y = round_up(np.max(y))
     max_x = round_up(np.max(x))
+    plt.title(title)
     plt.xlim([-0.01, max_x])
     plt.ylim([-0.01, max_y])
     plt.yscale('symlog')
@@ -98,14 +99,15 @@ def plot_cdfs(data, output_suffix):
     print 'CCDF (frequency) saved as %s' % output_filename
 
 
-def plot_loglog(data, output_suffix):
-    output_filename = 'loglog-' + output_suffix
+def plot_loglog(data, title):
+    output_filename = 'loglog-%s.pdf'
     plt.figure()
     x = range(len(data))
     y = data
     plt.plot(x, y, 'bo', markeredgecolor='k')
     max_y = round_up(np.max(y))
     max_x = round_up(np.max(x))
+    plt.title(title)
     plt.xlim([-0.01, max_x])
     plt.ylim([-0.01, max_y])
     plt.yscale('symlog')
@@ -122,40 +124,49 @@ def plot_loglog(data, output_suffix):
     print 'Log-log saved as %s.' % output_filename
 
 
-def main(input_snap_graph, is_directed, output_suffix):
-    fh = open(input_snap_graph, 'rb')
+def main(input_file, file_type, is_directed, title):
+    if file_type == 'SNAP':
+      fh = open(input_file, 'rb')
 
-    G = None
-    if is_directed:
-        G = nx.read_edgelist(fh, create_using=nx.DiGraph())
+      G = None
+      if is_directed:
+          G = nx.read_edgelist(fh, create_using=nx.DiGraph())
+      else:
+          G = nx.read_edgelist(fh)
+
+      fh.close()
+      keys, degrees = zip(*G.degree())
     else:
-        G = nx.read_edgelist(fh)
+      data = np.loadtxt(input_file, delimiter=' ', dtype='i8')
+      degrees = data[:,1]
 
-    fh.close()
-
-    # TODO: Maximum likehood fitting if we want to fit the exponent:
-    # https://cs.brynmawr.edu/Courses/cs380/spring2013/section02/slides/10_ScaleFreeNetworks.pdf
-    keys, degrees = zip(*G.degree())
     degree_dist = np.bincount(degrees)
 
-    plot_loglog(degree_dist, output_suffix)
-    plot_cdfs(degree_dist, output_suffix)
+#    plot_loglog(degree_dist, title)
+    plot_cdfs(degree_dist, title)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Plots degree distribution CDF, CCDF and log-log.')
-    parser.add_argument('-i', '--input_snap_graph',
-                        help='Relative path of input graph in SNAP format.',
-                        required=True)
+        description='Plots degree distribution CDF, CCDF and log-log for a given input SNAP graph, or input degrees file.')
+    parser.add_argument('-i', '--input_file',
+          help='Input file, which is either a graph in SNAP format (default) or a degrees file',
+          required=True)
     parser.add_argument('--is_directed',
-                        help='Treats input graph as directed (default False).',
-                        dest='is_directed',
-                        action='store_true')
+          help='Treats graph as directed (default False).',
+          dest='is_directed',
+          action='store_true')
     parser.set_defaults(is_directed=False)
-    parser.add_argument('-o', '--output_suffix',
-                        help='Suffix of output filename, saved in current dir.',
-                        required=True)
+    parser.add_argument('--title',
+          help='Plot title and also used as output file suffix.',
+          required=True)
+    parser.add_argument('--file_type',
+          const='SNAP',
+          nargs='?',
+          choices=['SNAP', 'degrees'],
+          help='Whether input file is either SNAP or a space separated list of "nodeid outdegree" entries',
+          required=True)
+
     args = parser.parse_args()
 
     main(**vars(args))
